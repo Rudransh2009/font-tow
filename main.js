d7f36a8f50a2655a59947e6272f48e7cf312be70
Rudransh2009 <73396134+Rudransh2009@users.noreply.github.com>
2021-05-15 21:03:18 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+function setup()
+{
+    video= createCapture(VIDEO);
+    video.size(550, 500);
+    
+
+    canvas = createCanvas(550, 500);
+    canvas.position(560,150);
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose',gotPose);
+}
+function draw()
+{
+    background('#9551e8');
+}
+function modelLoaded()
+{
+    console.log('poseNet Is Intialized!');
+}
